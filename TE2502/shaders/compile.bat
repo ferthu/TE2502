a915778c74b395d173de418d909e793a6cbb137c
Timmie Pettersson <timmie.pettersson@gmail.com>
2019-01-25 15:04:53 +0100

    Fix shader compilation file

--- TE2502/shaders/compile.bat
+++ TE2502/shaders/compile.bat
@@ -1,4 +1,5 @@
-del "compiled\" /q
+if exist "compiled\" del "compiled\" /q
+if not exist "compiled\" mkdir "compiled"
 
 for %%f in (*.glsl) do (
 @C:/VulkanSDK/1.1.82.1/Bin/glslangValidator.exe -V %%f -o compiled/%%f.spv

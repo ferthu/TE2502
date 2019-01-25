0d8bb06f355e1962f6a7ff84ec149cc377f03929
Timmie Pettersson <timmie.pettersson@gmail.com>
2019-01-25 21:31:45 +0100

    Use local glsl compiler exe

--- TE2502/shaders/compile.bat
+++ TE2502/shaders/compile.bat
@@ -2,6 +2,6 @@ if exist "compiled\" del "compiled\" /q
 if not exist "compiled\" mkdir "compiled"
 
 for %%f in (*.glsl) do (
-@C:/VulkanSDK/1.1.82.1/Bin/glslangValidator.exe -V %%f -o compiled/%%f.spv
+@./glslangValidator.exe -V %%f -o compiled/%%f.spv
 )
 pause

e496d2cdd476198a3af1f449760d4169d6a6d8e5
Eyzcode <fritjof.cavallin@gmail.com>
2019-02-21 14:11:49 +0100

    Derp

--- TE2502/src/application.hpp
+++ TE2502/src/application.hpp
@@ -113,9 +113,9 @@ private:
 	ComputeQueue m_ray_march_compute_queue;
 
 	// Error metric parameters
-	float m_em_area_multiplier = 0.5f;
-	float m_em_curvature_multiplier = 0.5f;
-	float m_em_threshold = 2.0f;
+	float m_em_area_multiplier = 0.f;
+	float m_em_curvature_multiplier = 0.f;
+	float m_em_threshold = 0.1f;
 
 	// Group size of error metric dispatch
 	uint32_t m_em_group_size = 0;
@@ -131,7 +131,7 @@ private:
 
 	bool m_show_imgui = true;
 	bool m_draw_ray_march = true;
-	bool m_draw_wireframe = false;
+	bool m_draw_wireframe = true;
 	bool m_triangulate = false;
 	bool m_triangulate_button_held = false;
 

4edc561ec5ffd0d9ea598807ee669f554ba00783
Eyzcode <fritjof.cavallin@gmail.com>
2019-04-12 13:44:55 +0200

    Basic generate multithreading

--- TE2502/src/cpu_triangulate.cpp
+++ TE2502/src/cpu_triangulate.cpp
@@ -1754,7 +1754,7 @@ namespace cputri
 			terrain_buffer->data[node_index].first_instance = 0;
 
 			terrain_buffer->data[node_index].vertex_count = 4;
-			terrain_buffer->data[node_index].new_points_count = GRID_SIDE * GRID_SIDE;
+			terrain_buffer->data[node_index].new_points_count = 0; //GRID_SIDE * GRID_SIDE;
 
 			terrain_buffer->data[node_index].min = min;
 			terrain_buffer->data[node_index].max = max;
@@ -1798,6 +1798,8 @@ namespace cputri
 			//terrain_buffer->data[node_index].border_triangle_indices[0] = 0;
 			//terrain_buffer->data[node_index].border_triangle_indices[1] = 1;
 		}
+
+		return;
 
 		//barrier();
 		//memoryBarrierBuffer();
